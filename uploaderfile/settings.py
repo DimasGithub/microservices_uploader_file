ae6e8b0eb0676d9710072e1699d29c616cf87a01
Dimas Setiawan <dim.dim10032000@gmail.com>
2022-12-14 15:14:04 +0700

    betulin admin dashboard assets

--- uploaderfile/settings.py
+++ uploaderfile/settings.py
@@ -107,6 +107,13 @@ REST_FRAMEWORK = {
     ],
 }
 
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_PATH, 'static'),
+)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
@@ -148,7 +155,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
